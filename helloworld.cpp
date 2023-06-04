78d50f4ef935ea8dd8670418313d828187f5b424
Leslieengr <leslieengr@gmail.com>
2023-06-05 05:11:58 +0700

    fistly

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+ int main(){
+
+    using namespace first;
+
+    pairlist_t pairlist;
+    
+
+    return 0;
+ }
